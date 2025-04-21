aec92419912732ef4552f304ef60e9001fd322d9
rftbtr <rifatbatir@gmail.com>
2025-04-22 00:10:04 +0300

    Files for TC_505 have been prepared.

--- src/technoStudy/tests/TC_505_SubmenuSocialMedia.java
+++ src/technoStudy/tests/TC_505_SubmenuSocialMedia.java
@@ -1,4 +1,17 @@
 package technoStudy.tests;
 
-public class TC_505_SubmenuSocialMedia {
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import technoStudy.base.BaseDriver;
+import technoStudy.methods.TC_505_Methods;
+
+public class TC_505_SubmenuSocialMedia extends BaseDriver{
+
+    @Test
+    @Parameters("BrowserType")
+    public void submenuSocialMediaTest(){
+        TC_505_Methods methods = new TC_505_Methods(driver, action, wait);
+        methods.submenuSocialMedia();
+
+    }
 }
