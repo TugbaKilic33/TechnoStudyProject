d4894d8917122097f16b6ed38fe52f3471275bf5
rftbtr <rifatbatir@gmail.com>
2025-04-21 16:27:35 +0300

    TC_505.gif added.

--- src/technoStudy/methods/TC_505_Methods.java
+++ src/technoStudy/methods/TC_505_Methods.java
@@ -1,4 +1,20 @@
 package technoStudy.methods;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import technoStudy.elements.ProjectAllElements;
+
 public class TC_505_Methods {
+    WebDriver driver;
+    Actions action;
+    WebDriverWait wait;
+    ProjectAllElements elements;
+
+    public TC_505_Methods(WebDriver driver, Actions action, WebDriverWait wait, ProjectAllElements elements) {
+        this.driver = driver;
+        this.action = action;
+        this.wait = wait;
+        this.elements = new ProjectAllElements(driver);
+    }
 }
