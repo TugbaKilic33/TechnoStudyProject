e20635badfc2c58e40b79d6f8a7f46468867d742
rftbtr <rifatbatir@gmail.com>
2025-04-22 14:39:25 +0300

    Completed, executed and tested “US_505 Submenu Social Media Test” code.

--- src/technoStudy/methods/TC_505_Methods.java
+++ src/technoStudy/methods/TC_505_Methods.java
@@ -1,9 +1,17 @@
 package technoStudy.methods;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import technoStudy.elements.ProjectAllElements;
+
+import java.util.List;
+import java.util.Set;
 
 public class TC_505_Methods {
     WebDriver driver;
@@ -17,11 +25,29 @@ public class TC_505_Methods {
         this.wait = wait;
         this.elements = new ProjectAllElements(driver);
     }
-    public void submenuSocialMedia(){
 
+    public void submenuSocialMedia() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        String technoStudyWindowID = driver.getWindowHandle();
+        elements.scrollIntoView(elements.socialLinkField);
+        String[] socialList = {"facebook", "instagram", "youtube", "linkedin"};
+        List<WebElement> socialLinkList = elements.socialLinks;
 
-
-
-
+        int i = 0;
+        for (WebElement social : socialLinkList) {
+            Assert.assertTrue(social.isDisplayed(), "Logo is not displayed");
+            social.click();
+            for (String windowHandle : driver.getWindowHandles()) {
+                if (!windowHandle.equals(technoStudyWindowID)) {
+                    driver.switchTo().window(windowHandle);
+                    break;
+                }
+            }
+            wait.until(ExpectedConditions.urlToBe(driver.getCurrentUrl()));
+            Assert.assertTrue(driver.getCurrentUrl().contains(socialList[i]), socialList[i]+" page not displayed!");
+            i++;
+            driver.close();
+            driver.switchTo().window(technoStudyWindowID);
+        }
     }
 }
